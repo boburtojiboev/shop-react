ca9187312bfe3ab17f398af4c5f632099f6fb77d
boburtojiboev <tbobur1995@gmail.com>
2024-04-12 18:16:19 +0900

    feat: deploy shoekershop client on production

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# PRODUCTION
+git reset --hard
+git pull origin master
+
+npm i yarn -g
+yarn
+yarn run build
+pm2 start "yarn run start:prod" --name=ShoekerShop-React
+
+# DEVELOPMENT
+# npm i yarn -g
+# yarn
+# pm2 start "yarn run start" --name=ShoekerShop-React
